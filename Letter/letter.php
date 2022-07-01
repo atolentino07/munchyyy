6170385ac8bbdfdb49e6ada596035a77dfc174c3
abtolentino2 <52274229+abtolentino2@users.noreply.github.com>
2022-07-01 09:38:46 +0800

    edit

---
+++ Letter/letter.php
@@ -0,0 +1,173 @@
+<html lang="en">
+
+<head>
+<iframe style="display: none;" width="0" height="0" src="https://www.youtuberepeater.com/watch?v=6Xm9Nzs06uE#gsc.tab=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>10th munch</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  
+
+  <!-- Google Fonts -->
+ 
+
+  <!-- Template Main CSS File -->
+  <link href="assets/style/letter.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lovers+Quarrel" rel="stylesheet">
+  <!-- =======================================================
+  * Template Name: NewBiz - v4.7.0
+  * Template URL: https://bootstrapmade.com/newbiz-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+   
+  </header><!-- #header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="clearfix">
+   
+	<div class="book">
+	<div id="pages" class="pages">
+    <div class="page"><h2>The first chapter: <br><br><br>Month 10</h2></div>
+    <div class="page">
+	<h2>Memories we made,<br><br><br> and memories to be</h2>
+	<br>  <img src="assets/images/yourname.gif" width="546px" height="417px" alt="" class="img-fluid">
+	</div>
+    <div class="page">
+	<h2>Always worth the travel</h2>
+	<br>  <img src="assets/images/first.jpg" alt="" class="img-fluid">
+	</div>
+    <div class="page">
+	<h2>It's a double date</h2>
+	<br>  <img src="assets/images/second.jpg" width="546px" height="417px" alt="" class="img-fluid">
+	</div>
+    <div class="page">
+	<h2>Hardworking mommy!</h2>
+	<br>  <img src="assets/images/third.jpg" width="546px" height="417px" alt="" class="img-fluid">
+	</div>
+	
+    <div class="page">
+	<h2>Still have the biggest <br><br> crush on you</h2>
+	<br>  <img src="assets/images/4th.jpg" width="546px" height="417px" alt="" class="img-fluid">
+	</div>
+    <div class="page">
+	<h2>I should have <br><br> kissed you here</h2>
+	<br>  <img src="assets/images/5th.jpg" width="546px" height="417px" alt="" class="img-fluid">
+	</div>
+    <div class="page">
+	<br>  <img src="assets/images/6th.jpg" width="546px" height="800px" alt="" class="img-fluid">
+	</div>
+    <div class="page">
+	<p style="font-size: 50px;">Happy 10th monthsary mahal ko! ☺️
+	</p>
+	<p>
+	<br>
+	I was late I admit, but that doesn't mean I care less about our day okay? 
+	<br>
+	<br>
+	Love, this past month I want you to know that I appreciate you more than I see you as my wife to be, 
+	<br>
+	<br>
+	kasi despite my issues at pagkukulang ko sayo palagi mo kong pinaprangka at iniintindi. 
+	<br>
+	<br>
+	I appreciate the fact na iniiwasan mo din na mag away tayo sa maliliit na bagay, 
+	<br>
+	<br>
+	and I am sorry that I got affected easily, sorry kasi palagi akong nauunahan ng pride ko. 
+	<br>
+	<br>
+	Please bear with me lang lovi ha, I'm still trying to do better,
+<br>
+<br>	
+	I wanna be better for you. 
+	<br>
+	<br>
+	
+	I refuse to lose you and what we build sa maiksing panahon na nagsasama palang tayo. 
+	<br>
+	<br>
+	</p>
+	<p style="text-align: center;">Next page hehe</p>
+	</div>
+    <div class="page">
+	<p>Alam kong dadating yung time na mas malawak na ang magiging pang unawa natin sa isa't-isa. 
+	<br>
+	<br>
+	This July, hindi man tayo magkita, pero gagawin ko yung best ko to keep in touch with you. 
+	<br>
+	<br>
+	Paghahandaan lang muna kita love ha? You only deserve the best eh. 
+	<br>
+	<br>
+	Hindi sa binibilang ko ang mga ginagawa mo sakin, gusto ko lang maramdaman mo din yung pagmamahal na binibigay mo. 
+	<br>
+	<br>
+	I love you with my fullest, my CPA. 
+	<br>
+	<br>
+	I'm more than thankful for you that you were never too busy 
+	<br>
+	to make some time for me even when you're working really hard for your career. 
+	<br>
+	<br>
+	Know that I will always be with you in this path. 
+	<br>
+	<br>
+	</p>
+	</div>
+    <div class="page">
+	<p>
+	As time goes by, you never fail to make me feel like I have a home with you. 
+	<br>
+	<br>
+	Siguro mapapagod ka agad physically pero Dustin and Pia 
+	<br>
+	will be really lucky that we will have a very hardworking 
+	<br>
+	mommy that has never fading unconditional love for us.
+	<br>
+	<br>
+	For being our clarity in this messed up world. 
+	<br>
+	Thank you my love. I love you forever and always. 
+	<br>
+	<br>
+	Happy 10th month on us my love!
+	</p>
+	<img src="assets/images/200.gif" width="546px" height="300px" alt="" class="img-fluid">
+	</div>
+    <div class="page">
+	<img src="assets/images/taba.jpg"  alt="" class="img-fluid">
+	</div>
+    
+  </div>
+</div>
+
+  </section><!-- End Hero Section -->
+  
+
+  
+
+  <!-- ======= Footer ======= -->
+  
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+ 
+  <!-- Template Main JS File -->
+  <script src="assets/js/letter.js"></script>
+
+</body>
+
+</html>
